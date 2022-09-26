bf2649ee3bc003f7bcc7d4ab63229cb680d8b100
dgnssena <1CFt2D41>
2022-09-26 19:46:38 +0300

    Proje dosyası ekle.

---
+++ ÖDEVVV/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace ÖDEVVV
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.lblYazi = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnHesapla.Location = new System.Drawing.Point(145, 272);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(104, 39);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "HESAPLA";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.SystemColors.Info;
+            this.btnTemizle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btnTemizle.Location = new System.Drawing.Point(158, 327);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(75, 23);
+            this.btnTemizle.TabIndex = 1;
+            this.btnTemizle.Text = "TEMİZLE";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.BackColor = System.Drawing.SystemColors.Info;
+            this.txtSayi.Location = new System.Drawing.Point(208, 124);
+            this.txtSayi.MinimumSize = new System.Drawing.Size(133, 33);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(133, 33);
+            this.txtSayi.TabIndex = 2;
+            this.txtSayi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSayi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSayi_KeyPress);
+            // 
+            // lblYazi
+            // 
+            this.lblYazi.AutoSize = true;
+            this.lblYazi.BackColor = System.Drawing.SystemColors.Info;
+            this.lblYazi.Location = new System.Drawing.Point(208, 197);
+            this.lblYazi.MinimumSize = new System.Drawing.Size(133, 33);
+            this.lblYazi.Name = "lblYazi";
+            this.lblYazi.Size = new System.Drawing.Size(133, 33);
+            this.lblYazi.TabIndex = 3;
+            this.lblYazi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblX.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblX.Location = new System.Drawing.Point(118, 124);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(31, 29);
+            this.lblX.TabIndex = 4;
+            this.lblX.Text = "X";
+            this.lblX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.BackColor = System.Drawing.Color.MediumPurple;
+            this.lblY.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblY.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblY.Location = new System.Drawing.Point(117, 197);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(30, 29);
+            this.lblY.TabIndex = 5;
+            this.lblY.Text = "Y";
+            this.lblY.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumPurple;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.lblYazi);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Label lblYazi;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+    }
+}
+
---
+++ ÖDEVVV/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ÖDEVVV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string onbin, bin, yuz, on, bir;
+
+        
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            txtSayi.Clear();
+
+            lblYazi.Text = String.Empty;
+        }
+
+        private void txtSayi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ',';
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double sayi = Convert.ToDouble(txtSayi.Text);
+
+            double onbinler, binler, yuzler, onlar, birler;
+
+          
+
+
+            birler = sayi % 10;
+            sayi = sayi - birler;
+            onlar = sayi % 100;
+            sayi = sayi - onlar;
+            yuzler = sayi % 1000;
+            sayi = sayi - yuzler;
+            binler = sayi % 10000;
+            onbinler = sayi - binler;
+
+            switch (onbinler)
+            {
+                case 00000: onbin = ""; break;
+                case 10000: onbin = "ON "; break;
+                case 20000: onbin = "YİRMİ "; break;
+                case 30000: onbin = "OTUZ "; break;
+                case 40000: onbin = "KIRK "; break;
+                case 50000: onbin = "ELLİ "; break;
+                case 60000: onbin = "ALTMIŞ "; break;
+                case 70000: onbin = "YETMİŞ "; break;
+                case 80000: onbin = "SEKSEN "; break;
+                case 90000: onbin = "DOKSAN "; break;
+            }
+            switch (binler)
+            {
+                case 0000: bin = ""; break;
+                case 1000: bin = " BİR BİN "; break;
+                case 2000: bin = "İKİ BİN "; break;
+                case 3000: bin = "ÜÇ BİN "; break;
+                case 4000: bin = "DÖRT BİN "; break;
+                case 5000: bin = "BEŞ BİN "; break;
+                case 6000: bin = "ALTI BİN "; break;
+                case 7000: bin = "YEDİ BİN "; break;
+                case 8000: bin = "SEKİZ BİN "; break;
+                case 9000: bin = "DOKUZ BİN "; break;
+            }
+            switch (yuzler)
+            {
+                case 000: yuz = ""; break;
+                case 100: yuz = "YÜZ "; break;
+                case 200: yuz = "İKİ YÜZ "; break;
+                case 300: yuz = "ÜÇ YÜZ "; break;
+                case 400: yuz = "DÖRT YÜZ "; break;
+                case 500: yuz = "BEŞ YÜZ "; break;
+                case 600: yuz = "ALTI YÜZ "; break;
+                case 700: yuz = "YEDİ YÜZ "; break;
+                case 800: yuz = "SEKİZ YÜZ "; break;
+                case 900: yuz = "DOKUZ YÜZ "; break;
+            }
+            switch (onlar)
+            {
+                case 00: on = ""; break;
+                case 10: on = "ON "; break;
+                case 20: on = "YİRMİ "; break;
+                case 30: on = "OTUZ "; break;
+                case 40: on = "KIRK "; break;
+                case 50: on = "ELLİ "; break;
+                case 60: on = "ALTMIŞ "; break;
+                case 70: on = "YETMİŞ "; break;
+                case 80: on = "SEKSEN "; break;
+                case 90: on = "DOKSAN "; break;
+            }
+            switch (birler)
+            {
+                case 0: bir = ""; break;
+                case 1: bir = "BİR "; break;
+                case 2: bir = "İKİ "; break;
+                case 3: bir = "ÜÇ "; break;
+                case 4: bir = "DÖRT "; break;
+                case 5: bir = "BEŞ "; break;
+                case 6: bir = "ALTI "; break;
+                case 7: bir = "YEDİ "; break;
+                case 8: bir = "SEKİZ "; break;
+                case 9: bir = "DOKUZ "; break;
+            }
+            sayi = Convert.ToDouble(txtSayi.Text);
+            
+
+            if (sayi <= 99999 && sayi >= 0.0001)
+            {
+                birler = sayi % 10;
+                if (birler == 0)
+                    lblYazi.Text = onbin + bin + yuz + on + "TL";
+                sayi = sayi - birler;
+
+                onlar = sayi % 100;
+                if (onlar == 0)
+                    lblYazi.Text = onbin + bin + yuz + bir + "TL" ;
+                sayi = sayi - onlar;
+
+                yuzler = sayi % 1000;
+                if (yuzler == 0)
+                    lblYazi.Text = onbin + bin + on + bir + "TL" ;
+
+                sayi = sayi - yuzler;
+
+                binler = sayi % 10000;
+                if (binler == 0)
+                    lblYazi.Text = onbin + "BİN " + yuz + on + bir + "TL" ;
+
+                if (onbinler == 0)
+                {
+                    if (binler == 1000)
+                        lblYazi.Text = "BİN " + yuz + on + bir + "TL" ;
+
+                    else
+                        lblYazi.Text = bin + yuz + on + bir + "TL" ;
+                }
+                if (birler != 0 && onlar != 0 && yuzler != 0 && binler != 0 && onbinler != 0)
+
+                    lblYazi.Text = onbin + bin + yuz + on + bir + "TL" ;
+
+            }
+            else
+                MessageBox.Show("Beş taneden fazla rakam girmeyiniz");
+        }
+    }
+}
+    
+
